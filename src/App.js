bbb7e3ce296b70a1e46136f0a87c88fa79a1c8bd
Ramiro Cerdá <rami992009@gmail.com>
2020-04-14 14:50:54 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./components/Logo";
+
+function App() {
+  return (
+    <div className="container center">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+
+const Logo = () => {
+  return (
+    <img
+      style={{  maxWidth: "85vw" }}
+      src={logo}
+      alt="breaking bad logo"
+    />
+  );
+};
+
+export default Logo;
